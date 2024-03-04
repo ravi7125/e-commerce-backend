10f80c2e92179c748d4837c7518d6059fe54c735
ztlab111 <ravip@zignuts.com>
2024-03-04 11:52:27 +0530

    change status code of already exists

--- config/constants.js
+++ config/constants.js
@@ -2,9 +2,9 @@ module.exports = {
     HTTP_STATUS: {
       SUCCESS: 200,
       CREATED: 201,
+      ALREADY_EXISTS: 203,
       BAD_REQUEST: 400,
       UNAUTHORIZED: 401,
-      ALREADY_EXISTS: 403,
       NOT_FOUND: 404,
       SERVER_ERROR: 500,
       FORBIDDEN: 403
